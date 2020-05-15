067c45be82377aebc6faf3293d9b064571d35423
brett.palmberg <brett.t.palmberg@usace.army.mil>
2020-05-15 14:03:01 -0700

    Working with database writes; done

--- python/lambda/lambda_function.py
+++ python/lambda/lambda_function.py
@@ -5,12 +5,14 @@ import os
 import tempfile
 import logging
 import json
+import psycopg2
+import psycopg2.extras
 
 # set up logger
 logger = logging.getLogger()
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.CRITICAL)
 # commented out to avoid duplicate logs in lambda
-# logger.addHandler(logging.StreamHandler())
+logger.addHandler(logging.StreamHandler())
 
 # imported to test loading of libraries
 # from osgeo import gdal
@@ -36,9 +38,21 @@ def get_infile(bucket, key, filepath):
         else:
             raise
 
+def db_connection():
+    user = os.getenv('CUMULUS_DBUSER')
+    host = os.getenv('CUMULUS_DBHOST')
+    dbname = os.getenv('CUMULUS_DBNAME')
+    password = os.getenv('CUMULUS_DBPASS')
+    
+    return psycopg2.connect(
+        user=user,
+        host=host,
+        dbname=dbname,
+        password=password
+    )
 
 def get_infile_processor(name):
-    """Import library for processing a given filetype"""
+    """Import library for processing a given product_name"""
     
     processor = importlib.import_module(f'cumulus.processors.{name}')
     
@@ -64,16 +78,61 @@ def upload_file(file_name, bucket, object_name=None):
     try:
         response = s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
-        logging.error(e)
+        logger.error(e)
         return False
     return True
 
+def get_acquirables():
+    '''list of acquirables in the database'''
+    try:
+        conn = db_connection()
+        c = conn.cursor()
+        c.execute("SELECT name from acquirable")
+        rows = c.fetchall()
 
-def write_entry_to_database(fileinfo):
+    finally:
+        c.close()
+        conn.close()
     
-    logging.debug(f'Write to Database Not Implemented: {fileinfo["file"]}')
+    return [r[0] for r in rows]
+
+
+def get_products():
+    '''Map of <name>:<product_id> for all products in the database'''
     
-    return True
+    try:
+        conn = db_connection()
+        c = conn.cursor()
+        c.execute("SELECT name, id from product")
+        rows = c.fetchall()
+    finally:
+        c.close()
+        conn.close()
+    
+    return { r[0]: r[1] for r in rows}
+
+
+def write_database(entries):
+    
+    def dict_to_tuple(d):
+        return tuple([d['datetime'], d['file'], d['product_id']])
+    
+    values = [dict_to_tuple(e) for e in entries]
+
+    try:
+        conn = db_connection()
+        c = conn.cursor()
+        psycopg2.extras.execute_values(
+            c, "INSERT INTO productfile (datetime, file, product_id) VALUES %s", values,
+        )
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        c.close()
+        conn.close()
+    
+    return len(entries)
 
 
 def lambda_handler(event, context=None):
@@ -86,13 +145,20 @@ def lambda_handler(event, context=None):
         
         logger.info(f'Lambda triggered by Bucket {bucket}; Key {key}')
 
-        # # Filename and filetype
+        # # Filename and product_name
         pathparts = key.split('/')
-        filetype, filename = pathparts[1], pathparts[-1]
-        logger.info(f'Process filetype: {filetype}; file: {filename}')
+        acquirable_name, filename = pathparts[1], pathparts[-1]
+        logger.info(f'Process acquirable_name: {acquirable_name}; file: {filename}')
+
+        # Check if acquirable is valid in the database
+        acquirables = get_acquirables()
+        logger.info(f'valid acquirables in database: {acquirables}')
+        if acquirable_name not in acquirables:
+            logger.error(f'acquirable_name not in database: {acquirable_name}')
+            return []
 
         # Find library to unleash on file
-        processor = get_infile_processor(filetype)
+        processor = get_infile_processor(acquirable_name)
         logger.info(f'Using processor: {processor}')
        
         with tempfile.TemporaryDirectory() as td:
@@ -101,14 +167,29 @@ def lambda_handler(event, context=None):
             # Process the file and return a list of files
             outfiles = processor.process(_file, td)
             
+            # Keep track of successes to send as single database query at the end
+            successes = []
+            # Valid products in the database
+            product_map = get_products()
             for _f in outfiles:
-                # Write output files to different bucket
-                write_key = 'cumulus/{}/{}'.format(_f["filetype"], _f["file"].split("/")[-1])
-                upload_success = upload_file(
-                    _f["file"], WRITE_TO_BUCKET, write_key
-                )
-                # Write Productfile Entry to Database
-                if upload_success:
-                    write_entry_to_database(_f)
+                # See that we have a valid 
+                if _f["filetype"] in product_map.keys():
+                    # Write output files to different bucket
+                    write_key = 'cumulus/{}/{}'.format(_f["filetype"], _f["file"].split("/")[-1])
+                    upload_success = upload_file(
+                        _f["file"], WRITE_TO_BUCKET, write_key
+                    )
+                    # Write Productfile Entry to Database
+                    if upload_success:
+                        successes.append({
+                            "product_id": product_map[_f["filetype"]],
+                            "datetime": _f['datetime'],
+                            "file": write_key,
+                        })
             
-        return outfiles
+            count = write_database(successes)
+            
+        return {
+            "count": count,
+            "productfiles": successes
+        }
