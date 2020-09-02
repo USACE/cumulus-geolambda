dfd73f5503b9a2f986433149bf27c6c240adaf07
Brett Palmberg <29417036+brettpalmberg@users.noreply.github.com>
2020-09-01 23:55:44 -0500

    saving work in progress (#7)

--- build-and-test.sh
+++ build-and-test.sh
@@ -7,4 +7,7 @@
 # snodas_unmasked
 # ./test.sh ./test_json/nohrsc_snodas_unmasked.json
 # prism_ppt_early
-./test.sh ./mock_events/prism_ppt_early.json
+# ./test.sh ./mock_events/prism_ppt_early.json
+
+# RTMA Rapid Update
+./test.sh ./mock_events/ncep_rtma_ru_anl.json
