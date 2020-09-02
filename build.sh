82625449eb6daf3ffcd5b5e00678aa4101a3b1f1
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-09-02 11:27:44 -0500

    fix ci

--- build.sh
+++ build.sh
@@ -4,9 +4,13 @@ VERSION=$(cat VERSION)
 
 # Build GDAL Base Container
 docker build . -t rsgis/geolambda:${VERSION}
-docker run --rm -v ${PWD}:/home/geolambda -it rsgis/geolambda:${VERSION} package.sh
+docker run --rm \
+    -v ${PWD}:/home/geolambda \
+    -it rsgis/geolambda:${VERSION} package.sh
 
 # Use GDAL Base Container to Build
 # Container with Python Layer and Dependencies
 docker build python --build-arg VERSION=${VERSION} -t rsgis/geolambda:${VERSION}-python
-docker run -v ${PWD}/python:/home/geolambda -t rsgis/geolambda:${VERSION}-python package-python.sh
+docker run --rm \
+    -v ${PWD}/python:/home/geolambda \
+    -t rsgis/geolambda:${VERSION}-python package-python.sh
